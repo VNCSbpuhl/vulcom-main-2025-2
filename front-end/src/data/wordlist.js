c8df32babaf943d11258437f36bcd6c85133557b
VNCSbpuhl <viniciusor64@gmail.com>
2025-10-29 22:32:51 -0300

    (29/10) Início da implementação de ataque de força bruta

---
+++ front-end/src/data/wordlist.js
@@ -0,0 +1,14 @@
+// Substitua/atualize este arquivo pela wordlist oficial indicada em aula.
+// Mantemos alguns exemplos mínimos para teste local.
+
+const wordlist = [
+  '123456',
+  'admin',
+  'password',
+  'qwerty',
+  'Vulcom@DSM',
+]
+
+export default wordlist
+
+
